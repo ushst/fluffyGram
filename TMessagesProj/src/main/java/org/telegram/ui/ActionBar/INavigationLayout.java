391afd82d0e54f51a489a9183df81aba17a98f8b
ushastoe <k@ushst.ru>
2025-02-05 22:44:10 +0300

    [tech] fxi INavigationLayout.java

--- TMessagesProj/src/main/java/org/telegram/ui/ActionBar/INavigationLayout.java
+++ TMessagesProj/src/main/java/org/telegram/ui/ActionBar/INavigationLayout.java
@@ -306,6 +306,7 @@ public interface INavigationLayout {
         /**
          * @deprecated You should override {@link INavigationLayoutDelegate#needPresentFragment(INavigationLayout, NavigationParams)} for more fields
          */
+        @Deprecated
         default boolean needPresentFragment(BaseFragment fragment, boolean removeLast, boolean forceWithoutAnimation, INavigationLayout layout) {
             return true;
         }
