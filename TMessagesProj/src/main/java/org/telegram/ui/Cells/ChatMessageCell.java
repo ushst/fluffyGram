46662b49ba0f382402f1358588d8ec75a78c6e6f
Arseny271 <arsendmitr@gmail.com>
2026-07-16 16:38:26 +0400

    update to 12.8.3 (6922)

--- TMessagesProj/src/main/java/org/telegram/ui/Cells/ChatMessageCell.java
+++ TMessagesProj/src/main/java/org/telegram/ui/Cells/ChatMessageCell.java
@@ -102,6 +102,7 @@ import androidx.core.math.MathUtils;
 import org.telegram.PhoneFormat.PhoneFormat;
 import org.telegram.messenger.AccountInstance;
 import org.telegram.messenger.AndroidUtilities;
+import org.telegram.messenger.AppGlobalConfig;
 import org.telegram.messenger.ApplicationLoader;
 import org.telegram.messenger.BotForumHelper;
 import org.telegram.messenger.BotInlineKeyboard;
